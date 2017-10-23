3d2a69a90075c08e672d4481c494c26e10156656
haburitic <haburitic@gmail.com>
2017-10-22 19:45:02 -0500

    Se adicionan validaciones y nuevos formularios

---
+++ dist/3.chunk.js
@@ -0,0 +1,177 @@
+webpackJsonp([3,11],{
+
+/***/ 1034:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_router__ = __webpack_require__(38);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__dynamicreports_component__ = __webpack_require__(947);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__components_reports_reports_component__ = __webpack_require__(946);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return routing; });
+
+
+
+// noinspection TypeScriptValidateTypes
+var routes = [
+    {
+        path: '',
+        component: __WEBPACK_IMPORTED_MODULE_1__dynamicreports_component__["a" /* Dynamicreports */],
+        children: [
+            { path: 'reports', component: __WEBPACK_IMPORTED_MODULE_2__components_reports_reports_component__["a" /* Reports */] },
+        ]
+    }
+];
+var routing = __WEBPACK_IMPORTED_MODULE_0__angular_router__["a" /* RouterModule */].forChild(routes);
+//# sourceMappingURL=dynamicreports.routing.js.map
+
+/***/ }),
+
+/***/ 1054:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(3)(false);
+// imports
+
+
+// module
+exports.push([module.i, ":host /deep/ .status-button {\n  width: 60px; }\n", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ 1129:
+/***/ (function(module, exports) {
+
+module.exports = "<ba-card cardTitle=\"Bienvenido al sistema ge Gestión de Cuentas\" baCardClass=\"with-scroll\">\r\n\t<form class=\"form-horizontal\">\r\n\t\t<div class=\"container-fluid\">\r\n\t\t\t\r\n\t\t</div>\r\n\t</form>\r\n</ba-card>"
+
+/***/ }),
+
+/***/ 627:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_common__ = __webpack_require__(7);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__theme_nga_module__ = __webpack_require__(167);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_forms__ = __webpack_require__(11);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__ng_bootstrap_ng_bootstrap__ = __webpack_require__(397);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__dynamicreports_routing__ = __webpack_require__(1034);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__dynamicreports_component__ = __webpack_require__(947);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__components_reports_reports_component__ = __webpack_require__(946);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "DynamicreportsModule", function() { return DynamicreportsModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+
+
+
+var DynamicreportsModule = (function () {
+    function DynamicreportsModule() {
+    }
+    return DynamicreportsModule;
+}());
+DynamicreportsModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_1__angular_common__["CommonModule"],
+            __WEBPACK_IMPORTED_MODULE_3__angular_forms__["a" /* FormsModule */],
+            __WEBPACK_IMPORTED_MODULE_2__theme_nga_module__["a" /* NgaModule */],
+            __WEBPACK_IMPORTED_MODULE_4__ng_bootstrap_ng_bootstrap__["b" /* NgbModalModule */],
+            __WEBPACK_IMPORTED_MODULE_5__dynamicreports_routing__["a" /* routing */],
+        ],
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_6__dynamicreports_component__["a" /* Dynamicreports */],
+            __WEBPACK_IMPORTED_MODULE_7__components_reports_reports_component__["a" /* Reports */],
+        ],
+        entryComponents: [],
+        providers: []
+    })
+], DynamicreportsModule);
+
+//# sourceMappingURL=dynamicreports.module.js.map
+
+/***/ }),
+
+/***/ 946:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Reports; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var Reports = (function () {
+    function Reports() {
+    }
+    return Reports;
+}());
+Reports = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
+        selector: 'reports',
+        template: __webpack_require__(1129),
+        styles: [__webpack_require__(1054)]
+    }),
+    __metadata("design:paramtypes", [])
+], Reports);
+
+//# sourceMappingURL=reports.component.js.map
+
+/***/ }),
+
+/***/ 947:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Dynamicreports; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var Dynamicreports = (function () {
+    function Dynamicreports() {
+    }
+    return Dynamicreports;
+}());
+Dynamicreports = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
+        selector: 'dynamicreports',
+        template: "<router-outlet></router-outlet>"
+    }),
+    __metadata("design:paramtypes", [])
+], Dynamicreports);
+
+//# sourceMappingURL=dynamicreports.component.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=3.chunk.js.map
